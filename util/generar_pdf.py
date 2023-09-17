f6269d4e859a26c393ea0ea921db3d26e45d5412
gabrieljustdance <gabriel2043579@gmail.com>
2023-09-17 19:32:07 -0400

    creado el archivo de dependencias

--- util/generar_pdf.py
+++ util/generar_pdf.py
@@ -20,3 +20,24 @@ def generar_pdf_usuario():
   showinfo("EXITO", "Reporte en PDF generado exitosamente!")
 
 
+
+
+
+
+def generar_pdf_veh():    
+  path_wkhtmltopdf = r'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
+  config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+
+  env = Environment(loader=FileSystemLoader("template"))
+  usuario_template = env.get_template("reporte.html")
+
+  usuario = {
+    "cols": ["ID", "Nombre", "Apellido", "Telefono", "Correo", "Tipo de usuario"],
+    "rows": controlador_user.select_all()["msg"]
+  }
+
+  html = usuario_template.render(usuario)
+  pdfkit.from_string(html,'reporte.pdf', configuration=config)
+  showinfo("EXITO", "Reporte en PDF generado exitosamente!")
+
+
